c8b64d935332a71aed1386422201c5204e44f332
ShadowChild <shadychild12@gmail.com>
2020-07-02 20:30:52 +0100

    Move towards WPF

---
+++ Candor/CandorCore/CandorRefs.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Runtime.ConstrainedExecution;
+using log4net;
+using log4net.Config;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFileExtension="log4net", Watch=true)]
+[assembly: log4net.Config.AliasRepository("Candor")]
+
+namespace CandorCore
+{
+    public class CandorRefs
+    {
+        public static readonly ILog LOGGER = LogManager.GetLogger(typeof(CandorRefs));
+
+        public static void Init()
+        {
+            LOGGER.Info("LOGGING");
+        }
+    }
+}
