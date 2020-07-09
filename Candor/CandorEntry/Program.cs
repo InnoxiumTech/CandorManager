86379a6a5bf7a1a499ddf1bc4e74235f64b0e8ba
ShadowChild <shadychild12@gmail.com>
2020-07-09 21:04:58 +0100

    Added CnadorEntry project for the main entry point for the application

---
+++ Candor/CandorEntry/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CandorEntry
+{
+    class Program
+    {
+        /**
+         * Entry point for candor, all other projects will be dll's
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
